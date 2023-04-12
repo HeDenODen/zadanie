46bc42c5a89f9ddb141f66c37849744bd29a1549
jarli <jarli@DENODEN>
2023-04-12 18:26:41 +0200

    Добавьте файлы проекта.

---
+++ test/test.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "I love C++!\n\n";
+	cout << "\tI love C++!\n\n";
+	cout << "\t\tI love C++!\n\n";
+	cout << "\t\t\tI love C++!\n\n";
+	cout << "\t\t\t\tI love C++!\n\n";
+
+}
